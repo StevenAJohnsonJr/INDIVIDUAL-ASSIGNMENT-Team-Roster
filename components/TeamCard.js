7b18c1f0817410628be096e7f9564179783ce3af
StevenAJohnsonJr <sajohhnson187@gmail.com>
2023-04-24 15:25:25 -0500

    teams card

---
+++ components/TeamCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+// import { deleteSingleAuthor } from '../api/authorData';
+
+function TeamCard({ teamObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
+  const deleteThisTeam = () => {
+    if (window.confirm(`Delete ${teamObj.team_name}?`)) {
+      deleteSingleTeam(teamObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      {/* <Card.Img variant="top" src={authorObj.image} alt={authorObj.title} style={{ height: '400px' }} /> */}
+      <Card.Body>
+        <Card.Title>{teamObj.team_name}</Card.Title>
+         {/* DYNAMIC LINK TO VIEW THE AUTHOR DETAILS  */}
+        <Link href={`/teams/${teamObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">
+            VIEW
+          </Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE AUTHOR DETAILS  */}
+        <Link href={`/teams/edit/${teamObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisTeam} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+TeamCard.propTypes = {
+  teamObj: PropTypes.shape({
+    team_name: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default TeamCard;
