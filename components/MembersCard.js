38c8db53d746913b7cad21276f7d1255828df989
StevenAJohnsonJr <sajohhnson187@gmail.com>
2023-04-24 16:27:58 -0500

    4th commit

---
+++ components/MembersCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteMember } from '../API calls/members';
+
+function MemberCard({ memberObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
+  const deleteThisMember = () => {
+    if (window.confirm(`Delete this scrub ${memberObj.name}?`)) {
+      deleteMember(memberObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={memberObj.image} alt={memberObj.name} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{memberObj.name}</Card.Title>
+        <p className="card-text bold">{memberObj.role }</p>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/members/${memberObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/members/edit/${memberObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisMember} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+MemberCard.propTypes = {
+  memberObj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    role: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default MemberCard;
---
+++ pages/members/member.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import MemberCard from '../../components/MembersCard';
+import { getMembers } from '../../API calls/members';
+import { useAuth } from '../../utils/context/authContext';
+
+export default function Members() {
+  // TODO: Set a state for books
+  const [members, setMembers] = useState([]);
+
+  // TODO: Get user ID using useAuth Hook
+  const { user } = useAuth();
+
+  // TODO: create a function that makes the API call to get all the books
+  const getAllTheMembers = () => {
+    getMembers(user.uid).then(setMembers);
+  };
+
+  // TODO: make the call to the API to get all the books on component render
+  useEffect(() => {
+    getAllTheMembers();
+  }, []);
+
+  return (
+          
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over authors here using AuthorCard component */}
+        {members.map((member) => (
+          <MemberCard key={member.firebaseKey} memberObj={member} onUpdate={getAllTheMembers} />
+        ))}
+      </div>
+  );
+}
